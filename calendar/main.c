c1012700014374cf9ced9ad835e7ed5fab8f8fad
ANTJNSX <Antombomben@gmail.com>
2024-01-01 03:28:18 +0100

    Start on calendar project, plan written

---
+++ calendar/main.c
@@ -0,0 +1,18 @@
+/*
+   Print calendar month and the days accordingly(maybe api calls?)
+   
+   save data for days, inputs for special dates and if something happens that day, mark it with a "*" if it does.
+
+   save in file?
+   json?
+
+   */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
+
